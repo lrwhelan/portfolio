a6008540501b8fd514088d84e0203d402e93690b
Lauren <lrwhelan1@gmail.com>
2019-05-17 19:39:52 -0230

    Updates

---
+++ precache-manifest.45f5949ce1a09aa626d612f3a229001a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce20088ba978ffb69470b3216caa5a94",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d4505e653475f5b7f3d8",
+    "url": "/portfolio/static/css/main.1c396ea4.chunk.css"
+  },
+  {
+    "revision": "03cd84c3d82ca44b9c69",
+    "url": "/portfolio/static/js/2.ec28daca.chunk.js"
+  },
+  {
+    "revision": "d4505e653475f5b7f3d8",
+    "url": "/portfolio/static/js/main.a5331e3b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
