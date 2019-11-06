1558ac8ffda26a10f8ee310bae4e8fdea7103c85
Lauren Whelan <lrwhelan1@gmail.com>
2019-11-05 22:47:07 -0500

    Update ExperiencePage.jsx

--- portfolio/src/ExperiencePage.jsx
+++ portfolio/src/ExperiencePage.jsx
@@ -35,29 +35,20 @@ class ExperiencePage extends Component {
                     <br />
                     <div>
                         I’ve developed the majority of the front-end of a web application in React (which also included
-                        implementing testing with Jest and Enzyme) for Nokia, and another in which I worked with a start-up
-                        company (who just recently got accepted to participate in Y Combinator - the first company to do so
-                        in Atlantic Canada!) and took on a full-stack role, implementing front-end features in vanilla
+                        implementing testing with Jest and Enzyme) for Nokia, and another in which I worked with CoLab Software
+                        and took on a full-stack development role, implementing front-end features in
                         JavaScript and back-end work in Python using Flask and SQLAlchemy.
                     </div>
                     <br />
                     <div>
-                        At my latest work term I also had the opportunity to share my React knowledge with a team of
-                        developers and helped create documentation and standards to facilitate the move of an application
-                        from vanilla JavaScript to React.
+                        I'm currently working as a Full-Stack Developer Co-op for Wind River Systems.
                     </div>
                     <br />
                     <div>
-                        At university, I’ve had the opportunity to program with languages like C++, Python and Java. For a
-                        software design project, myself and my partner created a mobile android application for pet
-                        adoption. Specifically, to allow people to view animals available for adoption in their area
-                        and connect them to shelters. This application was written in Java, and had a REST API written
-                        with PHP, served through Google Cloud.
+                        At university, I’ve had the opportunity to program with languages like C++, Python and Java.
                     </div>
                     <br />
                     <div>
-                        I’m in my second-last semester and currently completing courses in topics like Computer Security,
-                        Concurrent Programming and Digital Signal Processing.
                     </div>
                 </div>
             </div>
